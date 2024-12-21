cb468b9a54c9094082b4df4432e45ac4ccf30498
Marcos Brito <marcosbryto92@gmail.com>
2024-12-21 17:09:22 -0300

    initial Project

---
+++ src/main/java/com/marcosbrito/compass/semana8/automatize_test/AutomatizeTestApplication.java
@@ -0,0 +1,13 @@
+package com.marcosbrito.compass.semana8.automatize_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatizeTestApplication.class, args);
+    }
+
+}
