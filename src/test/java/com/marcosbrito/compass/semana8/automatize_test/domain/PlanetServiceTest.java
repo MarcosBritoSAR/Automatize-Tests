fa4fc65598eba10902dbc0c969e9c733b68bdc35
Marcos Brito <marcosbryto92@gmail.com>
2024-12-22 02:42:27 -0300

    -Usando o mockito
    	•	Usar @MockBean para criar uma versão fake de PlanetRepository.
    	•	Configurar o retorno esperado do mock com Mockito.when().
    	•	Seguir o padrão AAA (Arrange, Act, Assert) nos testes:
    	•	Arrange: Configurar mocks e dados.
    	•	Act: Executar o método a ser testado.
    	•	Assert: Verificar os resultados.

--- src/test/java/com/marcosbrito/compass/semana8/automatize_test/domain/PlanetServiceTest.java
+++ src/test/java/com/marcosbrito/compass/semana8/automatize_test/domain/PlanetServiceTest.java
@@ -1,32 +1,50 @@
 package com.marcosbrito.compass.semana8.automatize_test.domain;
 
 import static com.marcosbrito.compass.semana8.automatize_test.common.constantPlanet.PLANET;
+import static org.mockito.Mockito.when;
+
+import com.marcosbrito.compass.semana8.automatize_test.web.repository.PlanetRepository;
 import com.marcosbrito.compass.semana8.automatize_test.web.service.PlanetService;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest(classes = PlanetService.class) //informa ao spring que isso aqui é uma classe de teste e que precisa do uso de Beans
-@RequiredArgsConstructor
+@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
+//informa ao spring que isso aqui é uma classe de teste e que precisa do uso de Beans
 public class PlanetServiceTest {
     /*
     Vou precisar testar minha unidade de teste service.
      */
-    private final PlanetService planetService;
+    @Autowired
+    private PlanetService planetService;
 
-@Test
-    public void createPlanet_WithDataValid_restr_returnsPlanet(){
+    @MockBean
+    private PlanetRepository planetRepository;
 
-    //sut -> System Under Test
-    /*
-    Por padroes chamamos os nossos objeto de sut, que siguinifica
-        - Sistema sobre testes
-     */
 
-    Planet sut = planetService.create(PLANET);
+    @Test
+    public void createPlanet_WithDataValid_restr_returnsPlanet() {
+// AAA
 
-    Assertions.assertEquals(PLANET, sut);
+        //Arrange -> Arrumar os dados pro teste
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
+        //Act -> Pratico a ação de teste
+        Planet sut = planetService.create(PLANET);
+
+        //Assert -> Verifico o resutlado dos testes
+        Assertions.assertEquals(PLANET, sut);
 
     }
 
