ab1ff330c6d694b1b787fd5f2de62f6fb043c733
Marcos Brito <marcosbryto92@gmail.com>
2024-12-22 11:58:16 -0300

    Iniciando um teste de integração com implementação de um fake double
    - Usando o TestEntityManager para realizar uma consulta no H2 simulando a consulta no MySQL com o JpaRepository
    - Usando o @DataJpaTest para realizar as injeções de dependências

--- src/main/java/com/marcosbrito/compass/semana8/automatize_test/web/controller/PlanetController.java
+++ src/main/java/com/marcosbrito/compass/semana8/automatize_test/web/controller/PlanetController.java
@@ -3,12 +3,12 @@ package com.marcosbrito.compass.semana8.automatize_test.web.controller;
 import com.marcosbrito.compass.semana8.automatize_test.domain.Planet;
 import com.marcosbrito.compass.semana8.automatize_test.web.service.PlanetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/planet")
---
+++ src/test/java/com/marcosbrito/compass/semana8/automatize_test/domain/PlanetRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.marcosbrito.compass.semana8.automatize_test.domain;
+
+import static com.marcosbrito.compass.semana8.automatize_test.common.constantPlanet.PLANET;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.marcosbrito.compass.semana8.automatize_test.web.repository.PlanetRepository;
+
+/*
+ * Nota commit: Iniciando um teste de integração com implementacao de um fake doble
+ * 
+ * - Vou usar o TestEntityManager para realizar um consulta no h2 simulando a cosulta no MySql com o JpaRepository
+ * 
+ * - Vou usar o @DataJpaTest realizar as injecoes de dependencias
+ */
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithDataValid_returnsPlanet() {
+
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        assertNotNull(sut);
+
+        assertEquals(planet.getName(), sut.getName());
+        assertEquals(planet.getClimate(), sut.getClimate());
+        assertEquals(planet.getTerrain(), sut.getTerrain());
+
+    }
+
+}
